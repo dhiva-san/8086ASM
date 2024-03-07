e55c82c27bba5013ba69aa3232271bbf98bf5e94
dhiva-san <161754530+dhiva-san@users.noreply.github.com>
2024-03-07 19:47:59 +0530

    Create sqrt_8086MP.asm

---
+++ sqrt_8086MP.asm
@@ -0,0 +1,10 @@
+			mov bx,49d             ;move any perfect square number into the bx register
+			mov cx,0000h          
+loop1:mov ax,cx
+			mul cx
+			inc cx
+			cmp ax,bx
+			jne loop1
+			dec cx
+			mov [1500h],cx         ;the square root can be found in the address 1500h
+			hlt
